ce0f0aaa2a2fe87312c0dfa7f3a7a8a182429e4f
adeshazer123 <adeshazer@wisc.edu>
2024-07-16 13:22:47 +0900

    Add Procedure class

---
+++ thorlabs/cs165mu_procedure.py
@@ -0,0 +1,100 @@
+# Author: Amelie Deshazer 
+# Date: 2024-07-16 
+# Purpose: Camera procedure to control the camera 
+
+
+import logging 
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+#Author: Amelie Deshazer 
+#Date: 2024-07-03 
+#Purpose: Procedure for controlling keithley 2100 powermeter. 
+
+
+import logging 
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+import pandas as pd 
+import time 
+import c2 as cv2
+from time import sleep
+from pymeasure.log import console_log
+from pymeasure.instruments.keithley import Keithley2000
+from pymeasure.display.windows import ManagedWindow
+from pymeasure.experiment import Results, Worker, Procedure, FloatParameter, IntegerParameter
+from pymeasure.display import Plotter
+
+
+class Keithley2100Procedure(Procedure):
+    visa = 'USB0::0x05E6::0x2100::1149087::INSTR' #must add visa address
+    wait_time = FloatParameter('Time', units = 's', default = 0.01)
+    log.info(f"Wait_time initialized to {wait_time}")
+
+    DATA_COLUMNS = ['Time(s)', 'Voltage(V)']
+
+    def startup(self):
+        log.info("Starting up the Keithley 2100 powermeter...")
+        self.keithley = Keithley2000(self.visa) 
+        self.keithley.measure_voltage(0.01, ac = False)
+        sleep(self.wait_time) 
+        
+        #initialize the instrument
+        log.info("Starting up the measurement...")
+    def execute(self):
+        time_0 = time.time()
+        while True:
+            time_1 = time.time()
+            self.keithley.start_buffer()
+            dtime = time_1 - time_0 #measure time elapsed
+            data = {'Time (s)': dtime,
+                'Voltage(V)': self.keithley.voltage
+            }
+            print(data)
+
+            self.emit('results', data)
+            sleep(self.wait_time)
+            
+            if self.should_stop():
+                log.info("Stopping...")
+                break 
+    
+    def shutdown(self): 
+        pass
+class ManagedWindow(ManagedWindow):
+    def __init__(self): 
+        super().__init__(procedure_class = Keithley2100Procedure, 
+            inputs = ['wait_time'], 
+            displays = ['wait_time', 'voltage'], 
+            x_axis = 'Time (s)', 
+            y_axis = 'Voltage (V)'
+        )
+        self.setWindowTitle('Keithley 2100 Powermeter')
+
+if __name__ == '__main__':
+    console_log(log)
+    procedure = Keithley2100Procedure() #calling the class procedure
+    
+    data_filename = 'keithley2.csv'
+    log.info("Constructing the Results with a data file: %s" % data_filename)
+    results = Results(procedure, data_filename)
+    log.info("Results created")
+
+    plotter = Plotter(results) 
+    log.info("Plotter created")
+    plotter.start() 
+
+    log.info("Creating the worker...")
+    worker = Worker(results)
+    log.info("Worker created")
+    worker.start()
+    worker.join(timeout = 3600) #timeout in seconds
+    log.info("Measurement is finished.")
+
+class CovertRGBtoBGR:
+    def __init__(self):
+        pass
+    def covert(self, image):
+        return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
