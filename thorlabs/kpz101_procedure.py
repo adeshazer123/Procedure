59c7aabe68038da461ad43bf89c7fd4cd7a0bc11
nano713 <50872819+nano713@users.noreply.github.com>
2024-07-18 16:07:23 +0900

    Add MainWindow class, fix bugs in displays.

--- thorlabs/kpz101_procedure.py
+++ thorlabs/kpz101_procedure.py
@@ -4,13 +4,17 @@ import logging
 log = logging.getLogger(__name__)
 log.addHandler(logging.NullHandler())
 
+import sys
+
 import pandas as pd 
-from numpy import np
+import numpy as np
 from time import sleep
 from pymeasure.log import console_log
 from pymeasure.instruments.keithley import Keithley2000
 from pymeasure.instruments.thorlabs import KPZ101
 from pymeasure.display.windows import ManagedWindow
+from pymeasure.display.Qt import QtWidgets
+
 from pymeasure.experiment import Results, Worker, Procedure, FloatParameter, IntegerParameter
 from pymeasure.display import Plotter
 
@@ -20,13 +24,13 @@ class Keithley2100Procedure(Procedure):
     address = '29252556'
     wait_time = FloatParameter('Time(s)', units = 's', default = 0.1)
     #voltage_stage = FloatParameter('Voltage (V):Stage', units = 'V', default = 0.0)
-    start_freq = FloatParameter('Start Frequency', units = 'Hz', default = 0.0)
-    stop_freq = FloatParameter('Stop Frequency', units = 'Hz', default = 750)
+    start_voltage = FloatParameter('Start Voltage', units = 'Hz', default = 0.0)
+    stop_voltage = FloatParameter('Stop Frequency', units = 'Hz', default = 750)
     step_size = FloatParameter('Step Size', units = 'Hz', default = 0.266)
 
     log.info(f"Wait_time initialized to {wait_time}")
-    log.info(f"Start frequency initialized to {start_freq}")
-    log.info(f"Stop frequency initialized to {stop_freq}")
+    log.info(f"Start frequency initialized to {start_voltage}")
+    log.info(f"Stop frequency initialized to {stop_voltage}")
     log.info(f"Step size initialized to {step_size}")
 
     DATA_COLUMNS = ['Voltage(V):Stage', 'Voltage(V)']
@@ -61,33 +65,37 @@ class Keithley2100Procedure(Procedure):
     
     def shutdown(self): 
         pass
-class ManagedWindow(ManagedWindow):
+class MainWindow(ManagedWindow):
     def __init__(self): 
         super().__init__(procedure_class = Keithley2100Procedure, 
-            inputs = ['wait_time', 'start_freq', 'stop_freq', 'step_size'] 
-            displays = ['wait_time', 'start_freq', 'stop_freq', 'step_size',
-                        'voltage'], 
+            inputs = ['wait_time', 'start_voltage', 'stop_voltage', 'step_size'],
+            displays = ['wait_time', 'start_voltage', 'stop_voltage', 'step_size'], 
             x_axis = 'Voltage (V):Stage', 
             y_axis = 'Voltage (V)'
         )
         self.setWindowTitle('Measure Interference')
 
 if __name__ == '__main__':
-    console_log(log)
-    procedure = Keithley2100Procedure() #calling the class procedure
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())  
+
+    # console_log(log)
+    # procedure = Keithley2100Procedure() #calling the class procedure
     
-    data_filename = 'measure_interference.csv'
-    log.info("Constructing the Results with a data file: %s" % data_filename)
-    results = Results(procedure, data_filename)
-    log.info("Results created")
+    # data_filename = 'measure_interference.csv'
+    # log.info("Constructing the Results with a data file: %s" % data_filename)
+    # results = Results(procedure, data_filename)
+    # log.info("Results created")
 
-    plotter = Plotter(results) 
-    log.info("Plotter created")
-    plotter.start() 
+    # plotter = Plotter(results) 
+    # log.info("Plotter created")
+    # plotter.start() 
 
-    log.info("Creating the worker...")
-    worker = Worker(results)
-    log.info("Worker created")
-    worker.start()
-    worker.join(timeout = 3600) #timeout in seconds
-    log.info("Measurement is finished.")
+    # log.info("Creating the worker...")
+    # worker = Worker(results)
+    # log.info("Worker created")
+    # worker.start()
+    # worker.join(timeout = 3600) #timeout in seconds
+    # log.info("Measurement is finished.")
